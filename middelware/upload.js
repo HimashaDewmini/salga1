f7a40970667849d511b313210bfbf56f2b55914d
HimashaDewmini <himashadewmini12345@gmail.com>
2025-07-30 13:47:01 +0530

    upload.js update

--- middelware/upload.js
+++ middelware/upload.js
@@ -1,17 +1,43 @@
+// const multer = require('multer');
+// const path = require('path');
+
+// // Set up storage engine
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, path.join(__dirname, '../uploads'));
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, Date.now() + path.extname(file.originalname));
+//     }
+    
+// });
+
+// const upload = multer({ storage: storage });
+
+// module.exports = upload;
 const multer = require('multer');
 const path = require('path');
 
-// Set up storage engine
+// Storage config
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../uploads'));
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
-    
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Make sure /uploads exists
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  }
 });
 
-const upload = multer({ storage: storage });
+// File filter (optional)
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed!"), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
 
 module.exports = upload;
